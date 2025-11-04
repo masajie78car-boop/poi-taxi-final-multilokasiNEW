386b47e49048517f449650ef91431e325bfe8424
masajie78car-boop <masajie78car@gmail.com>
2025-11-05 05:03:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import fetch from "node-fetch";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, get } from "firebase/database";
 
